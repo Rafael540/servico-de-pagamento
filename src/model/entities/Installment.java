eb95a3f555204cd8111729ea1d084f09393267f5
Rafael Alves <alves540@hotmail.com>
2026-09-21 13:41:59 -0300

    Final commit

--- src/model/entities/Installment.java
+++ src/model/entities/Installment.java
@@ -1,25 +1,30 @@
 package model.entities;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Installment {
 
-	private Date dueDate;
+	private static DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+	
+	private LocalDate dueDate;
 	private Double amount;
 
 	
-	public Installment(Date dueDate, Double amount) {
+	public Installment(LocalDate dueDate, Double amount) {
 		this.dueDate = dueDate;
 		this.amount = amount;
 	}
 
 
-	public Date getDueDate() {
+	public LocalDate getDueDate() {
 		return dueDate;
 	}
 
 
-	public void setDueDate(Date dueDate) {
+	public void setDueDate(LocalDate dueDate) {
 		this.dueDate = dueDate;
 	}
 
@@ -31,6 +36,12 @@ public class Installment {
 
 	public void setAmount(Double amount) {
 		this.amount = amount;
+	}
+
+
+	@Override
+	public String toString() {
+		return dueDate.format(fmt) + " - " + String.format("%.2f",amount);
 	}
 	
 	
